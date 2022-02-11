a7862465656a0345c321368e7dd1c0e41181652c
Paloma Chaveiro <97704982+chaveiropaloma@users.noreply.github.com>
2022-02-11 08:15:26 -0300

    Some changes on my project

--- ongs.spec.js
+++ ongs.spec.js
@@ -4,7 +4,7 @@
 const { it } = require("mocha");
  
 describe('Ongs', () => { 
-    it('Deve poder realizar cadastro', () => {
+    it.skip('Deve poder realizar cadastro', () => {
         cy.visit('https://bethehero-frontend.netlify.app/register') 
         //cy.get - busca um elemento
         //.type - insere um texto
@@ -20,21 +20,43 @@ describe('Ongs', () => {
         //atribuir rota a alias
         //esperar com cy.wait e fazer uma validação
 
-    
+        
         cy.intercept('POST', '**/ongs').as('postOng');
+        
+        cy.get('[type="submit"]').click();
         cy.wait('@postOng').then((xhr) => {
-         expect(xhr.status).be.eq(200)
-      })
+            console.log(xhr)
+            console.log(xhr.response.statusCode)
+            expect(xhr.response.statusCode).be.equal(200);
+            expect(xhr.response.body).has.property('id');
+            expect(xhr.response.body.id).is.not.null;
+        });
 
-
-     
-
-
-        cy.get('[type="submit"]').click()
     });
+
     it('Deve poder realizar login', () => {
         
+        cy.request({
+            method: 'POST',
+            url: 'https://bethehero-back.herokuapp.com/ongs',
+            body: {
+                city: "Goiânia",
+                email: "onguii@gmail.com",
+                name: "Onguii",
+                uf: "GO",
+                whatsapp: "62988885555"
+                        }
+
+        }).then(response => {
+
+
+        });
     });
 });
 
+// city: "Goiânia"
+// email: "onguii@gmail.com"
+// name: "Onguii"
+// uf: "GO"
+// whatsapp: "62988885555"
 
